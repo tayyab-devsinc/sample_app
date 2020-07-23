88dd7c02733207b7e8199f7ef8ed075fac33cab8
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-07-23 19:00:02 +0500

    About pages added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
+    ActiveSupport::Deprecation.silenced = true # to disablee deprications logs
     render html: "Hello"
   end
 end
