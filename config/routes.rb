5734fec850bc2e4a8167d26372968a279e80d43d
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-07-24 21:34:13 +0500

    All error routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get '/404', to: 'erros#error_404'
-  get '/500', to: 'erros#error_500'
+  # get '/404', to: 'erros#error_404'
+  # get '/500', to: 'erros#error_500'
+
+  get '*unmatched_route', to: 'erros#error_404',
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
