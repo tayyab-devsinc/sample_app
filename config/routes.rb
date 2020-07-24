c36d5d3a957e83878de08716b5b5ccc6a14883d8
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-07-24 18:39:25 +0500

    Configured Error Routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get '/401', to: 'errors#erroe_401'
-  get '/402', to: 'errors#erroe_402'
-  get '/404', to: 'errors#erroe_404'
-  get '/500', to: 'errors#erroe_500'
+  get '/401', to: 'errors#error_401'
+  get '/402', to: 'errors#error_402'
+  get '/404', to: 'errors#error_404'
+  get '/500', to: 'errors#error_500'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
