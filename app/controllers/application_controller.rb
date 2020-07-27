3a8525e13b561cc7856d4e057f5901da4540b1d7
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-07-27 20:38:16 +0500

    Devise gem added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   def hello
     ActiveSupport::Deprecation.silenced = true # to disablee deprications logs
     render html: "Hello"
   end
-
-
 
 end
